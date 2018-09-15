5f50bfc5eb8826e1272ae8ed8dea05364d1d402f
DESKTOP-U7D79JB\KanHar <knaan.harpaz@gmail.com>
2018-09-15 23:50:23 +0300

    Bugfix and additions

--- KerasMps.py
+++ KerasMps.py
@@ -19,7 +19,7 @@ def factorial_decaying_random_init(shape):
 
 class MatrixPowerSeriesLayer(Layer):
     def __init__(self, length, **kwrags):
-        assert length > 0
+        assert length > 1
         self.length = length
         super().__init__(**kwrags)
 
@@ -51,12 +51,73 @@ class MatrixPowerSeriesLayer(Layer):
             tmp_imag = tf.einsum('ijk,ikl->ijl', tmp_real, x_imag) + tf.einsum('ijk,ikl->ijl', tmp_imag, x_real)
             tmp_real = new_tmp_real
 
-            res_imag += tmp_imag * coff_imag[i]
-            res_real += tmp_real * coff_real[i]
+            res_real += tmp_real * coff_real[i] - tmp_imag * coff_imag[i]
+            res_imag += tmp_imag * coff_real[i] + tmp_real * coff_real[i]
 
         res = tf.stack([res_real, res_imag], axis=1)
         return res
 
     def compute_output_shape(self, input_shape):
         return input_shape
+
+
+
+def factorial_decaying_random_initM(shape):
+    print(shape)
+    res = np.zeros(shape)
+    for i in range(shape[0]): # length
+        for k in range(shape[2]):
+            for l in range(shape[3]):
+                radius = random.random() / math.factorial(i)
+                theta = random.random() * 2 * math.pi
+                res[i,0,k,l] = radius*math.cos(theta)
+                res[i,1,k,l] = radius*math.sin(theta)
+    return res
+
+
+class MatrixMPowerSeriesLayer(Layer):
+    def __init__(self, length, **kwrags):
+        assert length > 1
+        self.length = length
+        super().__init__(**kwrags)
+
+
+    def build(self, input_shape):
+        self.coefficients = self.add_weight(name='coefficients',
+                                            shape=(self.length,*input_shape[1:]),
+                                            initializer=factorial_decaying_random_initM,
+                                            trainable=True)
+        self.unit = K.eye(input_shape[2])
+        super().build(input_shape)
+
+    def call(self, x):
+
+        x_real = x[:,0]
+        x_imag = x[:,1]
+
+        tmp_real = x_real
+        tmp_imag = x_imag
+
+        coff_real = self.coefficients[:,0]
+        coff_imag = self.coefficients[:,1]
+
+        res_real = tf.einsum('ij,jk->ik',self.unit, coff_real[0])
+        res_real += tf.einsum('ijk,kl->ijl', tmp_real, coff_real[1]) - tf.einsum('ijk,kl->ijl', tmp_imag, coff_imag[1])
+        res_imag = tf.einsum('ij,jk->ik',self.unit, coff_imag[0])
+        res_imag += tf.einsum('ijk,kl->ijl', tmp_real, coff_imag[1]) + tf.einsum('ijk,kl->ijl', tmp_imag, coff_real[1])
+        
+        for i in range(2, self.length):
+            new_tmp_real = tf.einsum('ijk,ikl->ijl', tmp_real, x_real) - tf.einsum('ijk,ikl->ijl', tmp_imag, x_imag)
+            tmp_imag = tf.einsum('ijk,ikl->ijl', tmp_real, x_imag) + tf.einsum('ijk,ikl->ijl', tmp_imag, x_real)
+            tmp_real = new_tmp_real
+
+            res_real += tmp_real * coff_real[i]
+            res_imag += tmp_imag * coff_imag[i]
+
+        res = tf.stack([res_real, res_imag], axis=1)
+        return res
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
 
--- testMps.py
+++ testMps.py
@@ -163,3 +163,162 @@ def test_exp():
     print(model.evaluate(ktest_data, ktest_labels, batch_size=10))
 
     return model
+
+
+# Matrix coefficients tests
+def test_zero_m():
+    data = np.vectorize(complex)(np.random.random((1000,10,10)),np.random.random((1000,10,10)))*2-1-1j
+    labels = []
+    for datum in data:
+        labels.append(np.zeros((10,10)))
+    labels = np.array(labels)
+
+    kdata = np.array(list(map(lambda x: [x.real, x.imag], data)))
+    klabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+    
+    test_data = np.vectorize(complex)(np.random.random((100,10,10)),np.random.random((100,10,10)))*2-1-1j
+    test_labels = []
+    for datum in test_data:
+        test_labels.append(np.zeros((10,10)))
+    test_labels = np.array(test_labels)
+
+    ktest_data = np.array(list(map(lambda x: [x.real, x.imag], test_data)))
+    ktest_labels = np.array(list(map(lambda x: [x.real, x.imag], test_labels)))
+
+    model = Sequential([
+        KerasMps.MatrixMPowerSeriesLayer(5, input_shape=(2,10,10))
+        ])
+
+    sgd = keras.optimizers.SGD(lr=0.1, decay=0.01, momentum=0.5, nesterov=True,clipnorm=100)
+    model.compile(optimizer=sgd,
+              loss='mse')
+
+    model.fit(kdata, klabels, epochs=1000, batch_size=128)
+    print(model.evaluate(ktest_data, ktest_labels, batch_size=128))
+
+    return model
+
+def test_unit_m():
+    data = np.vectorize(complex)(np.random.random((1000,10,10)),np.random.random((1000,10,10)))*2-1-1j
+    labels = []
+    for datum in data:
+        labels.append(np.identity(10))
+    labels = np.array(labels)
+
+    kdata = np.array(list(map(lambda x: [x.real, x.imag], data)))
+    klabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+    
+    test_data = np.vectorize(complex)(np.random.random((100,10,10)),np.random.random((100,10,10)))*2-1-1j
+    test_labels = []
+    for datum in test_data:
+        test_labels.append(np.identity(10))
+    test_labels = np.array(test_labels)
+
+    ktest_data = np.array(list(map(lambda x: [x.real, x.imag], test_data)))
+    ktest_labels = np.array(list(map(lambda x: [x.real, x.imag], test_labels)))
+
+    model = Sequential([
+        KerasMps.MatrixPowerSeriesLayer(5, input_shape=(2,10,10))
+        ])
+
+    sgd = keras.optimizers.SGD(lr=0.1, decay=0.01, momentum=0.5, nesterov=True,clipnorm=100)
+    model.compile(optimizer=sgd,
+              loss='mse')
+
+    model.fit(kdata, klabels, epochs=1000, batch_size=128)
+    print(model.evaluate(ktest_data, ktest_labels, batch_size=128))
+
+    return model
+
+
+def test_id_m():
+    data = np.vectorize(complex)(np.random.random((1000,10,10)),np.random.random((1000,10,10)))*2-1-1j
+    labels = data
+
+    kdata = np.array(list(map(lambda x: [x.real, x.imag], data)))
+    klabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+    
+    test_data = np.vectorize(complex)(np.random.random((100,10,10)),np.random.random((100,10,10)))*2-1-1j
+    test_labels = test_data
+
+    ktest_data = np.array(list(map(lambda x: [x.real, x.imag], test_data)))
+    ktest_labels = np.array(list(map(lambda x: [x.real, x.imag], test_labels)))
+
+    model = Sequential([
+        KerasMps.MatrixPowerSeriesLayer(5, input_shape=(2,10,10))
+        ])
+
+    sgd = keras.optimizers.SGD(lr=0.1, decay=0.01, momentum=0.5, nesterov=True,clipnorm=100)
+    model.compile(optimizer=sgd,
+              loss='mse')
+
+    model.fit(kdata, klabels, epochs=1000, batch_size=128)
+    print(model.evaluate(ktest_data, ktest_labels, batch_size=128))
+
+    return model
+
+
+def test_square_m():
+    data = np.vectorize(complex)(np.random.random((1000,10,10)),np.random.random((1000,10,10)))*2-1-1j
+    labels = []
+    for datum in data:
+        labels.append(datum.dot(datum))
+    labels = np.array(labels)
+
+    kdata = np.array(list(map(lambda x: [x.real, x.imag], data)))
+    klabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+    
+    test_data = np.vectorize(complex)(np.random.random((100,10,10)),np.random.random((100,10,10)))*2-1-1j
+    test_labels = []
+    for datum in test_data:
+        test_labels.append(datum.dot(datum))
+    test_labels = np.array(test_labels)
+
+    ktest_data = np.array(list(map(lambda x: [x.real, x.imag], test_data)))
+    ktest_labels = np.array(list(map(lambda x: [x.real, x.imag], test_labels)))
+
+    model = Sequential([
+        KerasMps.MatrixPowerSeriesLayer(5, input_shape=(2,10,10))
+        ])
+
+    sgd = keras.optimizers.SGD(lr=0.1, decay=0.01, momentum=0.5, nesterov=True,clipnorm=100)
+    model.compile(optimizer=sgd,
+              loss='mse')
+
+    model.fit(kdata, klabels, epochs=1000, batch_size=16)
+    print(model.evaluate(ktest_data, ktest_labels, batch_size=10))
+
+    return model
+
+
+def test_exp_m():
+    data = np.vectorize(complex)(np.random.random((1000,10,10)),np.random.random((1000,10,10)))*2-1-1j
+    labels = []
+    for datum in data:
+        labels.append(sp.linalg.expm(datum))
+    labels = np.array(labels)
+
+    kdata = np.array(list(map(lambda x: [x.real, x.imag], data)))
+    klabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+    
+    test_data = np.vectorize(complex)(np.random.random((100,10,10)),np.random.random((100,10,10)))*2-1-1j
+    test_labels = []
+    for datum in test_data:
+        test_labels.append(sp.linalg.expm(datum))
+    test_labels = np.array(test_labels)
+
+    ktest_data = np.array(list(map(lambda x: [x.real, x.imag], test_data)))
+    ktest_labels = np.array(list(map(lambda x: [x.real, x.imag], test_labels)))
+
+    model = Sequential([
+        KerasMps.MatrixPowerSeriesLayer(5, input_shape=(2,10,10))
+        ])
+
+    sgd = keras.optimizers.SGD(lr=0.1, decay=0.01, momentum=0.5, nesterov=True,clipnorm=100)
+    model.compile(optimizer=sgd,
+              loss='mse')
+
+    model.fit(kdata, klabels, epochs=1000, batch_size=16)
+    print(model.evaluate(ktest_data, ktest_labels, batch_size=10))
+
+    return model
