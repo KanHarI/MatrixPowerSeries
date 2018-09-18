6ab8d6af2da3094db328706124f756e84c1890bd
DESKTOP-U7D79JB\KanHar <knaan.harpaz@gmail.com>
2018-09-18 11:47:45 +0300

    Added support for multiple channels of input and output from the layer

--- KerasMps.py
+++ KerasMps.py
@@ -22,15 +22,15 @@ def factorial_decaying_random_init(shape):
 # Where X is a complex input matrix, and the coefficients a_n are complex.
 # The optimized weights of this layer are the coefficients
 class MatrixPowerSeriesLayer(Layer):
-    def __init__(self, length, **kwrags):
-        assert length > 1
-        self.length = length
+    def __init__(self, degree, **kwrags):
+        assert degree > 1
+        self.degree = degree
         super().__init__(**kwrags)
 
 
     def build(self, input_shape):
         self.coefficients = self.add_weight(name='coefficients',
-                                            shape=(self.length,2),
+                                            shape=(self.degree,2),
                                             initializer=factorial_decaying_random_init,
                                             trainable=True)
         self.unit = K.eye(input_shape[2])
@@ -53,7 +53,7 @@ class MatrixPowerSeriesLayer(Layer):
         res_real = self.unit * coff_real[0]
         res_imag = self.unit * coff_imag[0]
         
-        for i in range(1, self.length):
+        for i in range(1, self.degree):
             # Calculate a raise by one of the current power of the matrix
             # Remainder: (a+bj)(c+dj)=(ac-bd)+(ad+bc)j, and the same 
             # formulas hold for matrices
@@ -76,7 +76,7 @@ class MatrixPowerSeriesLayer(Layer):
 
 def factorial_decaying_random_initM(shape):
     res = np.zeros(shape)
-    for i in range(shape[0]): # length
+    for i in range(shape[0]): # degree
         # initiate matrix as a random multiple of the unit matrix
         radius = random.random() / math.factorial(i)
         theta = random.random() * 2 * math.pi
@@ -106,15 +106,15 @@ def multi_factorial_decaying_random_initM(shape):
 # Where X is a complex input matrix, and the coefficients A_n are complex matrices.
 # The optimized weights of this layer are the coefficients
 class MatrixMPowerSeriesLayer(Layer):
-    def __init__(self, length, **kwrags):
-        assert length > 1
-        self.length = length
+    def __init__(self, degree, **kwrags):
+        assert degree > 1
+        self.degree = degree
         super().__init__(**kwrags)
 
 
     def build(self, input_shape):
         self.lcoefficients = self.add_weight(name='lrcoefficients',
-                                            shape=(self.length,*input_shape[1:]),
+                                            shape=(self.degree,*input_shape[1:]),
                                             initializer=factorial_decaying_random_initM,
                                             trainable=True)
         self.unit = K.eye(input_shape[2])
@@ -135,7 +135,7 @@ class MatrixMPowerSeriesLayer(Layer):
         res_real = tf.einsum('jk,kl->jl', lcoff_real[0], self.unit)
         res_imag = tf.einsum('jk,kl->jl', lcoff_imag[0], self.unit)
         
-        for i in range(1, self.length):
+        for i in range(1, self.degree):
             new_tmp_real = tf.einsum('ijk,ikl->ijl', tmp_real, x_real) - tf.einsum('ijk,ikl->ijl', tmp_imag, x_imag)
             tmp_imag = tf.einsum('ijk,ikl->ijl', tmp_real, x_imag) + tf.einsum('ijk,ikl->ijl', tmp_imag, x_real)
             tmp_real = new_tmp_real
@@ -155,15 +155,15 @@ class MatrixMPowerSeriesLayer(Layer):
 # Where X is a complex input matrix, and the coefficients A_n, B_n are complex matrices.
 # The optimized weights of this layer are the coefficients
 class MatrixM2PowerSeriesLayer(Layer):
-    def __init__(self, length, **kwrags):
-        assert length > 1
-        self.length = length
+    def __init__(self, degree, **kwrags):
+        assert degree > 1
+        self.degree = degree
         super().__init__(**kwrags)
 
 
     def build(self, input_shape):
         self.lrcoefficients = self.add_weight(name='lrcoefficients',
-                                            shape=(self.length,2,*input_shape[1:]),
+                                            shape=(self.degree,2,*input_shape[1:]),
                                             initializer=multi_factorial_decaying_random_initM,
                                             trainable=True)
         self.lcoefficients = self.lrcoefficients[:,0]
@@ -190,13 +190,13 @@ class MatrixM2PowerSeriesLayer(Layer):
         res_real = tf.einsum('jk,kl->jl', self.unit, rcoff_real[0])
         res_imag = tf.einsum('jk,kl->jl', self.unit, rcoff_imag[0])
         
-        for i in range(1, self.length):
+        for i in range(1, self.degree):
             new_tmp_real = tf.einsum('ijk,ikl->ijl', tmp_real, x_real) - tf.einsum('ijk,ikl->ijl', tmp_imag, x_imag)
             tmp_imag = tf.einsum('ijk,ikl->ijl', tmp_real, x_imag) + tf.einsum('ijk,ikl->ijl', tmp_imag, x_real)
             tmp_real = new_tmp_real
 
             # Multiply by right coefficient
-            # Temporary results of right multiplication only to keep line length managable
+            # Temporary results of right multiplication only to keep line degree managable
             rmul_real = tf.einsum('ijk,kl->ijl', tmp_real, rcoff_real[i]) - tf.einsum('ijk,kl->ijl', tmp_imag, rcoff_imag[i])
             rmul_imag = tf.einsum('ijk,kl->ijl', tmp_real, rcoff_imag[i]) + tf.einsum('ijk,kl->ijl', tmp_imag, rcoff_real[i])
 
@@ -214,23 +214,23 @@ class MatrixM2PowerSeriesLayer(Layer):
 def multi_factorial_decaying_random_init(shape):
     res = []
     for i in range(shape[0]):
-        if len(shape[1:]) == 3:
+        if len(shape[1:]) == 2:
             res.append(factorial_decaying_random_init(shape[1:]))
         else:
             res.append(multi_factorial_decaying_random_init(shape[1:]))
     return np.array(res)
 
 class MultichannelMatrixPowerSeriesLayer(Layer):
-    def __init__(self, length, out_channels, **kwrags):
-        assert length > 1
-        self.length = length
+    def __init__(self, degree, out_channels, **kwrags):
+        assert degree > 1
+        self.degree = degree
         self.out_channels = out_channels
         super().__init__(**kwrags)
 
 
     def build(self, input_shape):
         self.coefficients = self.add_weight(name='coefficients',
-                                            shape=(self.length,self.out_channels,2),
+                                            shape=(self.degree,self.out_channels,2),
                                             initializer=multi_factorial_decaying_random_init,
                                             trainable=True)
         self.unit = K.eye(input_shape[-1])
@@ -261,11 +261,11 @@ class MultichannelMatrixPowerSeriesLayer(Layer):
         coff_imag = self.coefficients[:,:,1]
         # coff_real/imag is now a matrix [o,j]
 
-        res_real = tf.einsum('j,mn->jmn', coff_real[0], self.unit)
-        res_imag = tf.einsum('j,mn->jmn', coff_imag[0], self.unit)
+        res_real = tf.einsum('j,k,mn->jkmn', coff_real[0], tf.ones((x.shape[1],)), self.unit)
+        res_imag = tf.einsum('j,k,mn->jkmn', coff_imag[0], tf.ones((x.shape[1],)), self.unit)
 
         
-        for o in range(1, self.length):
+        for o in range(1, self.degree):
             new_tmp_real = tf.einsum('ikmn,iknl->ikml', tmp_real, x_real) - tf.einsum('ikmn,iknl->ikml', tmp_imag, x_imag)
             tmp_imag = tf.einsum('ikmn,iknl->ikml', tmp_real, x_imag) + tf.einsum('ikmn,iknl->ikml', tmp_imag, x_real)
             tmp_real = new_tmp_real
@@ -279,4 +279,4 @@ class MultichannelMatrixPowerSeriesLayer(Layer):
         return res
 
     def compute_output_shape(self, input_shape):
-        return input_shape
+        return (input_shape[0], self.out_channels, *input_shape[1:])
--- testMps.py
+++ testMps.py
@@ -8,12 +8,8 @@ import KerasMps
 
 LR = 1
 DECAY = 0.25
-BATCH_SIZE = 256
 
-# func - the function approximation being tested
-# layer - the layer being tested
-def generic_test_scalar(func, layer, epochs=100, samples=10000, test_samples=100, matrix_size=10, degree=5):
-    # Creating training dataset
+def generate_samples(func, matrix_size, samples):
     # Initialize source matrix with random elements in the [-1,-1j]X[1,1j]
     # square on the complex plane
     data = np.vectorize(complex)(
@@ -27,55 +23,120 @@ def generic_test_scalar(func, layer, epochs=100, samples=10000, test_samples=100
     labels = np.array(labels)
 
     # Seprate into real and complex parts
-    kdata = np.array(list(map(lambda x: [x.real, x.imag], data)))
-    klabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+    ridata = np.array(list(map(lambda x: [x.real, x.imag], data)))
+    rilabels = np.array(list(map(lambda x: [x.real, x.imag], labels)))
+
+    return (ridata,rilabels)
+
+# func - the function approximation being tested
+# layer - the layer being tested
+def generic_test_scalar(func, layer, epochs=100, samples=5000, test_samples=100, matrix_size=6, degree=4, batch_size=256):
+    # Create training and test datasets
+    train_data, train_labels = generate_samples(func, matrix_size, samples)
+    test_data, test_labels = generate_samples(func, matrix_size, test_samples)
     
-    # Craeting test dataset in the same way
-    test_data = np.vectorize(complex)(
-        np.random.random((test_samples,matrix_size,matrix_size)),
-        np.random.random((test_samples,matrix_size,matrix_size)))*2-1-1j
-
-    test_labels = []
-    for datum in test_data:
-        test_labels.append(func(datum))
-    test_labels = np.array(test_labels)
-
-    ktest_data = np.array(list(map(lambda x: [x.real, x.imag], test_data)))
-    ktest_labels = np.array(list(map(lambda x: [x.real, x.imag], test_labels)))
-
     # Create model
     model = Sequential([
-        layer(degree, input_shape=(2,matrix_size,matrix_size))
+        layer(degree=degree, input_shape=(2,matrix_size,matrix_size))
         ])
 
     opt = keras.optimizers.RMSprop(lr=LR, decay=DECAY)
     model.compile(optimizer=opt,
                     loss='logcosh')
-    model.fit(kdata, klabels, epochs=epochs, batch_size=BATCH_SIZE)
+    model.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
 
-    return (model.evaluate(ktest_data, ktest_labels, batch_size=BATCH_SIZE), model)
+    return (model.evaluate(test_data, test_labels, batch_size=batch_size), model)
 
 
 
-test_zero = lambda **kwargs: generic_test_scalar(lambda x: np.zeros(x.shape), KerasMps.MatrixPowerSeriesLayer, **kwargs)
-test_unit = lambda **kwargs: generic_test_scalar(lambda x: np.identity(x.shape[0]), KerasMps.MatrixPowerSeriesLayer, **kwargs)
-test_id = lambda **kwargs: generic_test_scalar(lambda x: x, KerasMps.MatrixPowerSeriesLayer, **kwargs)
-test_square = lambda **kwargs: generic_test_scalar(lambda x: np.matmul(x,x), KerasMps.MatrixPowerSeriesLayer, **kwargs)
-test_exp = lambda **kwargs: generic_test_scalar(lambda x: sp.linalg.expm(x), KerasMps.MatrixPowerSeriesLayer, **kwargs)
+zero = lambda x: np.zeros(x.shape)
+unit = lambda x: np.identity(x.shape[0])
+identity = lambda x: x
+square = lambda x: np.matmul(x,x)
+expm = sp.linalg.expm
+
+
+test_zero = lambda **kwargs: generic_test_scalar(zero, KerasMps.MatrixPowerSeriesLayer, **kwargs)
+test_unit = lambda **kwargs: generic_test_scalar(unit, KerasMps.MatrixPowerSeriesLayer, **kwargs)
+test_id = lambda **kwargs: generic_test_scalar(identity, KerasMps.MatrixPowerSeriesLayer, **kwargs)
+test_square = lambda **kwargs: generic_test_scalar(square, KerasMps.MatrixPowerSeriesLayer, **kwargs)
+test_exp = lambda **kwargs: generic_test_scalar(expm, KerasMps.MatrixPowerSeriesLayer, **kwargs)
 
 # For some reason these converge much slower than the m2 variant
-test_zero_m = lambda **kwargs: generic_test_scalar(lambda x: np.zeros(x.shape), KerasMps.MatrixMPowerSeriesLayer, **kwargs)
-test_unit_m = lambda **kwargs: generic_test_scalar(lambda x: np.identity(x.shape[0]), KerasMps.MatrixMPowerSeriesLayer, **kwargs)
-test_id_m = lambda **kwargs: generic_test_scalar(lambda x: x, KerasMps.MatrixMPowerSeriesLayer, **kwargs)
-test_square_m = lambda **kwargs: generic_test_scalar(lambda x: np.matmul(x,x), KerasMps.MatrixMPowerSeriesLayer, **kwargs)
-test_exp_m = lambda **kwargs: generic_test_scalar(lambda x: sp.linalg.expm(x), KerasMps.MatrixMPowerSeriesLayer, **kwargs)
+test_zero_m = lambda **kwargs: generic_test_scalar(zero, KerasMps.MatrixMPowerSeriesLayer, **kwargs)
+test_unit_m = lambda **kwargs: generic_test_scalar(unit, KerasMps.MatrixMPowerSeriesLayer, **kwargs)
+test_id_m = lambda **kwargs: generic_test_scalar(identity, KerasMps.MatrixMPowerSeriesLayer, **kwargs)
+test_square_m = lambda **kwargs: generic_test_scalar(square, KerasMps.MatrixMPowerSeriesLayer, **kwargs)
+test_exp_m = lambda **kwargs: generic_test_scalar(expm, KerasMps.MatrixMPowerSeriesLayer, **kwargs)
 
 # For some reason these converge much faster than the previous
-test_zero_m2 = lambda **kwargs: generic_test_scalar(lambda x: np.zeros(x.shape), KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
-test_unit_m2 = lambda **kwargs: generic_test_scalar(lambda x: np.identity(x.shape[0]), KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
-test_id_m2 = lambda **kwargs: generic_test_scalar(lambda x: x, KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
-test_square_m2 = lambda **kwargs: generic_test_scalar(lambda x: np.matmul(x,x), KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
-test_exp_m2 = lambda **kwargs: generic_test_scalar(lambda x: sp.linalg.expm(x), KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
+test_zero_m2 = lambda **kwargs: generic_test_scalar(zero, KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
+test_unit_m2 = lambda **kwargs: generic_test_scalar(unit, KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
+test_id_m2 = lambda **kwargs: generic_test_scalar(identity, KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
+test_square_m2 = lambda **kwargs: generic_test_scalar(square, KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
+test_exp_m2 = lambda **kwargs: generic_test_scalar(expm, KerasMps.MatrixM2PowerSeriesLayer, **kwargs)
 
 
-# 
+# convention:
+# i is batch size
+# j is output channels
+# k is input channels
+# l is 0/1 - real/complex part
+# m,n are elements of the input matrix
+# o - degree of polynomial
+def multichannel_generate_samples(func, matrix_size, input_channels, samples):
+
+    # Initialize source matrix with random elements in the [-1,-1j]X[1,1j]
+    # square on the complex plane
+    data = np.vectorize(complex)(
+        np.random.random((samples, input_channels, matrix_size, matrix_size)),
+        np.random.random((samples, input_channels, matrix_size, matrix_size)))*2-1-1j
+
+    labels = []
+    for channel in data:
+        f_input_channels = []
+        for datum in channel:
+            f_input_channels.append(func(datum))
+        labels.append(f_input_channels)
+    labels = np.array(labels)
+
+    ridata = np.einsum('likmn->iklmn', np.array([data.real, data.imag]))
+    rilabels = np.einsum('likjmn->ijklmn', np.array([labels.real, labels.imag]))
+
+    return (ridata,rilabels)
+
+def generic_multichannel_test(func,
+                            layer,
+                            out_channels,
+                            input_channels=5,
+                            epochs=100,
+                            samples=1000,
+                            test_samples=100,
+                            matrix_size=6,
+                            degree=4,
+                            batch_size=256):
+    train_data, train_labels = multichannel_generate_samples(func, matrix_size, input_channels, samples)
+    test_data, test_labels = multichannel_generate_samples(func, matrix_size, input_channels, test_samples)
+
+    model = Sequential([
+        layer(
+            degree=degree,
+            out_channels=out_channels,
+            input_shape=(input_channels,2,matrix_size,matrix_size)) # input_shape=[k,l,m,n]
+        # output_shape: [j,k,l,m,n]
+        ])
+
+    opt = keras.optimizers.RMSprop(lr=LR, decay=DECAY)
+    model.compile(optimizer=opt,
+                    loss='logcosh')
+    model.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
+
+    return (model.evaluate(test_data, test_labels, batch_size=batch_size), model)
+
+zero_unit_id_square = lambda x: np.array([zero(x),unit(x),identity(x),square(x)])
+
+test_multichannel = lambda **kwargs: generic_multichannel_test(
+    func=zero_unit_id_square,
+    layer=KerasMps.MultichannelMatrixPowerSeriesLayer,
+    out_channels=4,
+    **kwargs)
